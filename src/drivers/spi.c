5681155cf97d838deee1809fece444d984835f2a
Valoeghese <valoeghesemods@gmail.com>
2024-10-27 10:49:28 +1300

    fix: Proper spi pins

--- src/drivers/spi.c
+++ src/drivers/spi.c
@@ -1,11 +1,19 @@
 #include <stdint.h>
 #include <avr/io.h>
 
+// SPI on ATmega328p
+
+#define MOSI PB3
+#define MISO PB4
+#define SCLK PB5
+
 void spi_init(void)
 {
     // Set up data direction for SPI pins
     DDRB |= (1 << MOSI) | (1 << SCLK);
     DDRB &= ~(1 << MISO);
+    // Set up SPI registers
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0); // enable spi, master, prescaler 16
 }
 
 void spi_select(int8_t port)
@@ -18,10 +26,19 @@ void spi_select(int8_t port)
     }
 
     pin_selected = port;
-    SPI_PORT &= ~(1 << port);
+
+    if (port) {
+        SPI_PORT &= ~(1 << port);
+    }
 }
 
 void spi_transmit(char c)
 {
-    
+    /* Start transmission */
+    SPDR = cData;
+
+
+    /* Wait for transmission complete */
+    while(!(SPSR & (1<<SPIF)))
+        ;
 }
--- src/drivers/spi.h
+++ src/drivers/spi.h
@@ -1,17 +1,10 @@
 /*
  * spi.h
  * Header for Serial-Peripheral Interface.
- * SPI is set up on SPI_PORT, which should be defined in your buildscript.
- * SPI locations:
- * - MOSI / PB0
- * - 
+ * SPI chip select is set up on SPI_PORT, which should be defined in your buildscript.
  */
 
 #include <stdint.h>
-
-#define MOSI PB0
-#define MISO PB1
-#define SCLK PB2
 
 void spi_init(void); // set up SPI
 void spi_select(int8_t port);
